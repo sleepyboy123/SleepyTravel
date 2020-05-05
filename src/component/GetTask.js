1133fb127bb3dba19ddd5ecf24ded45cbe80d17b
sleepyboy <sleepyboy123@hotmail.com>
2020-05-05 18:54:16 +1000

    Added CRUD

--- src/component/GetTask.js
+++ src/component/GetTask.js
@@ -5,41 +5,69 @@ class GetTask extends React.Component {
     constructor() {
         super();
         this.state = {
-            tasks: []
+            tasks: [],
+            username: ""
         }
     }
-
-    componentDidMount() {
+    
+    fetchTasks = e => {
         const db = firebase.firestore();
-        db.collection('tasks').where("Username", "==", "sleepyboy").get()
+        e.preventDefault();
+        db.collection('tasks').where("Username", "==", this.state.username).get()
         .then(querySnapshot => {
             const data = querySnapshot.docs.map( doc => doc.data());
             this.setState({
-                tasks: data
+                tasks: data,
+                username: ""
+            })
+        })
+    }
+
+    updateInput = e => {
+        this.setState({
+            username: e.target.value
+        })
+    }
+
+    deleteTask(username, taskName, taskDescription) {
+        const db = firebase.firestore();
+        db.collection('tasks').where("Username", "==", username).where("TaskName", "==", taskName).where("TaskDescription", "==", taskDescription).get()
+        .then((querySnapshot) => {
+            querySnapshot.forEach((doc) => {
+                doc.ref.delete();
             })
         })
     }
 
     render() {
-        
         return (
             <div>
-                <table>
-                    <tr>
-                        <th>Username</th>
-                        <th>Task Name</th>
-                        <th>Task Description</th>
-                    </tr>
-                    {this.state.tasks.map(item => {
-                        return (
-                            <tr>
-                                <td>{item.Username}</td>
-                                <td>{item.TaskName}</td>
-                                <td>{item.TaskDescription}</td>
-                            </tr>
-                        )
-                    })}
-                </table>
+                <form onSubmit={this.fetchTasks}>
+                        <input type="text" name="Username" placeholer="Username" onChange={this.updateInput} value={this.state.username}/>
+                        <button type="submit">Search</button>
+                    </form>
+                {this.state.tasks.length === 0 ? <div></div> : 
+                <div>
+                    <table>
+                        <tr>
+                            <th>Username</th>
+                            <th>Task Name</th>
+                            <th>Task Description</th>
+                        </tr>
+                        {this.state.tasks.map(item => {
+                            return (
+                                <tr>
+                                    <td>{item.Username}</td>
+                                    <td>{item.TaskName}</td>
+                                    <td>{item.TaskDescription}</td>
+                                    <button onClick={() => {this.deleteTask(item.Username, item.TaskName, item.TaskDescription)}}>Delete</button>
+                                </tr>
+                            )
+                        })}
+                    </table>
+                </div>
+                }
+                
             </div>
         )
     }
