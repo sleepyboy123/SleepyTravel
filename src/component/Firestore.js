c3187b68ab272566659e042c91b2890f55583505
sleepyboy <sleepyboy123@hotmail.com>
2020-05-05 14:23:42 +1000

    Moved API Key to config.js

--- src/component/Firestore.js
+++ src/component/Firestore.js
@@ -1,18 +1,8 @@
 import firebase from 'firebase';
-
-var firebaseConfig = {
-    apiKey: "AIzaSyAaoIe39DMjHh6LhzEs4-JZmeTwMgMrH1g",
-    authDomain: "sleepytask-eca80.firebaseapp.com",
-    databaseURL: "https://sleepytask-eca80.firebaseio.com",
-    projectId: "sleepytask-eca80",
-    storageBucket: "sleepytask-eca80.appspot.com",
-    messagingSenderId: "119195353652",
-    appId: "1:119195353652:web:ba13b81c1654c9709fc061",
-    measurementId: "G-JL0XF8STBP"
-};
+import { myConfig } from '../config';
 
 // Initialize Firebase
-firebase.initializeApp(firebaseConfig);
+firebase.initializeApp(myConfig);
 firebase.analytics();
 
 export default firebase;
